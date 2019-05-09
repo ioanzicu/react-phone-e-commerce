2d6247b13cd3d2cca3bfb68c5ca293730f3d842b
Ioan Zicu <ioan.zicu94@gmail.com>
2019-05-09 19:24:46 +0200

    ProductList Component with ContextAPI

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
+import { storeProducts } from "../data";
+import { ProductConsumer } from "../components/context";
 
 class ProductList extends Component {
   state = {
-    products: []
+    products: storeProducts
   };
 
   render() {
@@ -13,11 +15,19 @@ class ProductList extends Component {
         <div className="py-5">
           <div className="container">
             <Title name="our" title="products" />
-            <div className="row" />
+
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
           </div>
         </div>
       </React.Fragment>
-      // <Product />
     );
   }
 }
