4384511afdb5743d87273664f116acdda3f6f32f
Ioan Zicu <ioan.zicu94@gmail.com>
2019-05-11 19:45:07 +0200

    clearCart() Functionality

--- src/components/context.js
+++ src/components/context.js
@@ -82,7 +82,17 @@ class ProductProvider extends Component {
     console.log("item removed");
   };
   clearCart = () => {
-    console.log("cart was cleared");
+    this.setState(
+      () => {
+        return {
+          cart: []
+        };
+      },
+      () => {
+        this.setProducts();
+        this.addTotals();
+      }
+    );
   };
   addTotals = () => {
     let subTotal = 0;
